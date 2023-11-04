5d5929a1d61f450867c59715d3bbc17f8b81e6b4
Rupeshkumar23 <coolrupesh2000@gmail.com>
2023-11-04 15:27:13 +0530

    update

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React, { useState } from "react";
-import { Card, Button } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown, faCaretUp,faXmark} from '@fortawesome/free-solid-svg-icons';
 function Tour({ tour, removeTour }) {
   const [showInfo, setShowInfo] = useState(false);
 
   const handleRemoveTour = () => {
-    // eslint-disable-next-line react/prop-types
     removeTour(tour.id);
   };
 
@@ -17,28 +16,26 @@ function Tour({ tour, removeTour }) {
       <div className="img-container">
         <Card.Img variant="top" src={tour.img} alt="Pic" className="Img_card" />
         <span className="close-btn" onClick={handleRemoveTour}>
-          <i className="fa-solid fa-xmark"></i>
+        <FontAwesomeIcon icon={faXmark} />
         </span>
       </div>
       <Card.Body className="tour-info">
-        <Card.Title className="mb-2 text-danger bg-white p-1">
-          {tour.city}
-        </Card.Title>
-        <Card.Subtitle className="mb-2 text-white bg-danger p-1">
-          {tour.name}
-        </Card.Subtitle>
-        <Card.Text className="para">{showInfo && <p>{tour.info}</p>}</Card.Text>
+        <Card.Title className="mb-2 text-danger bg-white p-1">{tour.city}</Card.Title>
+        <Card.Subtitle className="mb-2 text-white bg-danger p-1">{tour.name}</Card.Subtitle>
+        <Card.Text className='para'>
+          {showInfo && <p>{tour.info}</p>}
+        </Card.Text>
         <Button className="Btn" onClick={() => setShowInfo(!showInfo)}>
-          {showInfo ? (
-            <span>
-              Hide Info <FontAwesomeIcon icon={faCaretUp} />
-            </span>
-          ) : (
-            <span>
-              Show Info <FontAwesomeIcon icon={faCaretDown} />
-            </span>
-          )}
-        </Button>
+      {showInfo ? (
+        <span>
+         Hide Info  <FontAwesomeIcon icon={faCaretUp} />
+        </span>
+      ) : (
+        <span>
+           Show Info <FontAwesomeIcon icon={faCaretDown} />
+        </span>
+      )}
+    </Button>
       </Card.Body>
     </Card>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
