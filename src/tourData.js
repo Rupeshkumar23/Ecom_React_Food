f5b1c87e4cf62daba6d862a01fc559fe34f50aa9
Rupeshkumar23 <coolrupesh2000@gmail.com>
2023-11-04 17:44:14 +0530

    up

--- src/tourData.js
+++ src/tourData.js
@@ -2,7 +2,7 @@ export const tourData = [
   {
     id: 1,
     city: "New York",
-    img: "./Imgs/Pizza.jpg",
+    img: require('./Imgs/Pizza.jpg'),
     name: "New York Style Pizza",
     info: "New York-style pizza is known for its thin, foldable crust, with a wide range of toppings. It's a must-try when in NYC.",
   },
