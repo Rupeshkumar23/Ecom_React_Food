3d89e5cad3d91df92c94e3fad43f2bcda35ae7ef
Rupeshkumar23 <coolrupesh2000@gmail.com>
2023-11-04 19:49:05 +0530

    update

--- src/tourData.js
+++ src/tourData.js
@@ -2,28 +2,28 @@ export const tourData = [
   {
     id: 1,
     city: "New York",
-    img: "https://images.pexels.com/photos/15564188/pexels-photo-15564188/free-photo-of-pancakes-with-berries-and-marple-syrup.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    img: "/Pizza.jpg", // Update the image path to start from the public directory
     name: "New York Style Pizza",
     info: "New York-style pizza is known for its thin, foldable crust, with a wide range of toppings. It's a must-try when in NYC.",
   },
   {
     id: 2,
     city: "India",
-    img: "../public/Biryani.jpg",
+    img: "/Biryani.jpg", // Update the image path to start from the public directory
     name: "Biryani",
     info: "Biryani is a popular Indian dish made with aromatic rice, meat (such as chicken or lamb), and a blend of spices. It's a flavorful and fragrant dish.",
   },
   {
     id: 3,
     city: "London",
-    img: "../public/Fish and Chips.jpg",
+    img: "/Fish and Chips.jpg", // Update the image path to start from the public directory
     name: "Fish and Chips",
     info: "Fish and chips is a classic British dish consisting of battered and deep-fried fish served with crispy fries. It's a staple in London.",
   },
   {
     id: 4,
     city: "Paris",
-    img: "../public/Croissant.jpg",
+    img: "/Croissant.jpg", // Update the image path to start from the public directory
     name: "Croissant",
     info: "The croissant is a beloved French pastry known for its buttery and flaky layers. It's perfect for a Parisian breakfast or snack.",
   },
